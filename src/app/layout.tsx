ff7360b6c326e8ba90958c4c1b6482f13cdada58
lukesuv <lukesuvaal@gmail.com>
2024-02-20 09:14:24 +0100

    new blocks below

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Navbar />
+
           <main className="">{children}</main>
           <Footer />
         </ThemeProvider>
